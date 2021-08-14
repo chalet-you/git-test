683ceda998564ddf82270ad02fd8187c1eb427ce
chalet-you <937259669@qq.com>
2021-08-14 13:39:19 +0000

    update src/main/java/com/panting/git/GitTest.java.

--- src/main/java/com/panting/git/GitTest.java
+++ src/main/java/com/panting/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("push test1");
         System.out.println("pull test1");
         System.out.println("gitee test1");
+        System.out.println("gitee test2");
     }
 }
